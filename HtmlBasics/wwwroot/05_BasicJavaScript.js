4a9ea7fd1e514c60ecfe44af52f5d1f3773ef8d8
mehmet anaç <mehmetanac3@gmail.com>
2022-10-30 14:36:42 +0300

    Add project files.

---
+++ HtmlBasics/wwwroot/05_BasicJavaScript.js
@@ -0,0 +1,10 @@
+﻿function adSoyadBirlestir() {
+    var adTextBox = document.getElementById("tbAd");
+    var ad = adTextBox.value;
+
+    var soyadTextBox = document.getElementById("tbSoyad");
+    var soyad = soyadTextBox.value;
+    var adsoyadP = document.getElementsByClassName("sonuc")[0];
+    console.log(adTextBox);
+    adsoyadP.innerHTML = ad + " " + soyad; 
+}
